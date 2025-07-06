03911295327ab76634d47a4bb690a071d7e57505
AxisCoords <themid.hamid@gmail.com>
2025-07-07 00:51:59 +0200

    Nothing too crazy

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main() {
 	CreateWindow(WIN_WIDTH, WIN_HEIGHT, "GAME");
 
-	entt::Player* player = new entt::Player((Vector2){8 * 4, WIN_HEIGHT / 2});
+	entt::Player* player = new entt::Player((Vector2){WIN_WIDTH / 2, WIN_HEIGHT / 2});
 	
 	while (!WindowShouldClose()) {
 		if (IsKeyPressed(KEY_F3)) showDebug = !showDebug;
@@ -18,22 +18,22 @@ int main() {
 			player->Render(WHITE);
 
 			/* #region Debug */
-		if (showDebug) {
-			DrawFPS(0, 0);
+			if (showDebug) {
+				DrawFPS(0, 0);
 
-			DrawLineV(
-				(Vector2){0, WIN_HEIGHT / 2}, 
-				(Vector2){WIN_WIDTH, WIN_HEIGHT / 2}, 
-				RED
-			);
+				DrawLineV(
+					(Vector2){0, WIN_HEIGHT / 2}, 
+					(Vector2){WIN_WIDTH, WIN_HEIGHT / 2}, 
+					RED
+				);
 
-			DrawLineV(
-				(Vector2){WIN_WIDTH / 2, 0}, 
-				(Vector2){WIN_WIDTH / 2, WIN_HEIGHT}, 
-				GREEN
-			);
-		}
-		/* #endregion */
+				DrawLineV(
+					(Vector2){WIN_WIDTH / 2, 0}, 
+					(Vector2){WIN_WIDTH / 2, WIN_HEIGHT}, 
+					GREEN
+				);
+			}
+			/* #endregion */
 		EndDrawing();
 	}
 
